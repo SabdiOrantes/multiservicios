fd93d9bf0847af3c2752427038274d74e611d2f9
carlos Miranda <crm.giron@gmail.com>
2020-09-30 03:24:16 -0600

    Menú de Compras

--- TallerAnalisis/Controllers/HomeController.cs
+++ TallerAnalisis/Controllers/HomeController.cs
@@ -38,7 +38,12 @@ namespace TallerAnalisis.Controllers
             return View();
         }
 
-            [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Compra()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
