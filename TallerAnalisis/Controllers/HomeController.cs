cb91581a3ef380e082bd3de962b50e8f94fad07c
SabdiOrantes <sorantes@gmail.com>
2020-10-01 18:49:59 -0600

    Rportes

--- TallerAnalisis/Controllers/HomeController.cs
+++ TallerAnalisis/Controllers/HomeController.cs
@@ -38,7 +38,13 @@ namespace TallerAnalisis.Controllers
             return View();
         }
 
-            [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Reportes()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
